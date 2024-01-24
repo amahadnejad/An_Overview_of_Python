f4e7f2825ea5fedda3b8d1f5bf9d4afa66374130
Amir <amahadnezhad@gmail.com>
2024-01-24 20:08:23 +0330

    For Loops

---
+++ for_loop.py
@@ -0,0 +1,33 @@
+"""
+    For Loop
+    To Run Use This In Terminal --> python for_loop.py
+"""
+# Example 1
+# fruits = ['apple', 'banana', 'cherry', 'kiwi']
+# for fruit in fruits:
+#     print(fruit)
+
+# Example 2
+# numbers = range(1, 10)
+# for i in numbers:
+#     print(i)
+# or
+# for i in range(1, 10):
+#     print(i)
+#
+# for i in "banana":
+#     print(i)
+
+
+# fruits = ['apple', 'banana', 'cherry', 'kiwi']
+# for fruit in fruits:
+#     print(fruit)
+#     if fruit == 'cherry':
+#         break
+
+
+fruits = ['apple', 'banana', 'cherry', 'kiwi']
+for fruit in fruits:
+    print(fruit)
+    if fruit == 'cherry':
+        continue
