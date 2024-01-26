8106ecf7aa50f1358e4222f0ecfbb51167e5a418
Amir <amahadnezhad@gmail.com>
2024-01-26 18:54:47 +0330

    MATH, Working With Math Module

---
+++ math.py
@@ -0,0 +1,33 @@
+"""
+Math Class
+Python has a set of built-in math functions, including an extensive math module, that allows you to perform mathematical tasks on numbers.
+"""
+import math
+
+# Min
+x = min(3, 9, 5)
+print(x)
+
+# Max
+x = max(10, 8, 19)
+print(x)
+
+# Power
+x = pow(2, 3)
+print(x)
+
+# SQRT
+x = math.sqrt(16)
+print(x)
+
+# PI Number
+x = math.pi
+print(x)
+
+# Ceil --> Rounds a number up to the nearest integer
+x = math.ceil(4.26)
+print(x)
+
+# Floor --> Rounds a number down to the nearest integer
+x = math.floor(3.89)
+print(x)
