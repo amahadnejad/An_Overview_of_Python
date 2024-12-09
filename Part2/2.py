9624e96369aa9d06e5c84505abefb8a7805a5dbc
amahadnezhad <amahadnezhad@gmail.com>
2024-12-09 16:49:51 +0330

    Use dis Module

--- Part2/2.py
+++ Part2/2.py
@@ -1,9 +1,10 @@
 from timeit import timeit
+from dis import dis
 
 # Comprehension Vs Iter
 
 def iter():
-    [x for x in range(100) if x > 10]
+    return [x for x in range(100) if x > 10]
 
 
 def loop():
@@ -19,9 +20,22 @@ def loop_fast():
         if x > 10:
             pass
 
-print("Iter Func: ")
-print(timeit('iter()', setup='from __main__ import iter'))
-print("Loop Func: ")
-print(timeit('loop()', setup='from __main__ import loop'))
-print("Fast Loop Func: ")
-print(timeit('loop_fast()', setup='from __main__ import loop_fast'))
+# print("Iter Func: ")
+# print(timeit('iter()', setup='from __main__ import iter'))
+# print("Loop Func: ")
+# print(timeit('loop()', setup='from __main__ import loop'))
+# print("Fast Loop Func: ")
+# print(timeit('loop_fast()', setup='from __main__ import loop_fast'))
+
+def my_func(alist):
+    return len(alist)
+
+print(dis(my_func))
+
+
+
+
+
+
+
+
