56553959b00f210e03df017878a52b270ab53a3b
amahadnezhad <amahadnezhad@gmail.com>
2024-12-15 13:29:09 +0330

    Use WhiteSpaces

--- Part2/4.py
+++ Part2/4.py
@@ -6,3 +6,21 @@ Else In Comprehension
 x = [x if x in 'aeiov' else '*' for x in 'apple']
 # print(x)
 
+
+def foo(i):
+    return i, i + .5
+
+# for i in range(3):
+#     for x in foo(i):
+#         print(str(x))
+
+# Whitespaces
+a = [
+    str(x)
+    for i in range(3)
+        for x in foo(i)]
+
+print(a)
+
+
+
