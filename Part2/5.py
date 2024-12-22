15a9250708bf03ea1153a6ffe995ae1b47cd5b5b
amahadnezhad <amahadnezhad@gmail.com>
2024-12-22 16:49:13 +0330

    Comprehension vs Map Filter And XRange

--- Part2/5.py
+++ Part2/5.py
@@ -1,3 +1,11 @@
-a = [sorted(x) for x in [[2, 1], [4, 3], [0, 1]]]
-print(a)
+# a = [sorted(x) for x in [[2, 1], [4, 3], [0, 1]]]
+# print(a)
+
+
+def myfunc():
+    [x*x for x in range(1, 10) if x == 2]
+
+
+def ufunc():
+    list(filter(lambda x: x == 2, map(lambda x: x*x, range(1, 10))))
 
